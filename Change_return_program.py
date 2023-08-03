7f30bc8086e76a148e50c78b0f661ba5845c6480
excaliburrrin <136586193+excaliburrrin@users.noreply.github.com>
2023-08-03 13:27:04 +0500

    Update Change_return_program.py

--- Change_return_program.py
+++ Change_return_program.py
@@ -1,7 +1,15 @@
-a = int(input('mani pulim '))
+//dd a = int(input('mani pulim '))
 b = int(input('sotvoganim '))
 c = a - b
 if b > a:
+    print('qarzim',c)
+else:
+    print('qaytim', c) //
+    
+a = int(input('mani pulim '))
+b = int(input('sotvoganim '))
+c = a - b
+if c < 0:
     print('qarzim',c)
 else:
     print('qaytim', c)
